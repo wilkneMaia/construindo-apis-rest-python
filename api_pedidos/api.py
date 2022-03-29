6370c1e9cc4380e5b983ec94447f9dd0ddd9473f
Wilkne Maia <wilkne@outlook.com>
2022-03-29 14:51:31 -0300

    Adiciona documentação ao projeto

--- api_pedidos/api.py
+++ api_pedidos/api.py
@@ -4,7 +4,7 @@ from uuid import UUID
 from fastapi import Depends, FastAPI, Request
 from fastapi.responses import JSONResponse
 
-from api_pedidos.esquema import Item
+from api_pedidos.esquema import HealthCheckResponse, Item
 from api_pedidos.excecao import (FalhaDeComunicacaoError,
                                  PedidoNaoEncontradoError)
 
@@ -20,16 +20,27 @@ def tratar_erro_pedido_nao_encontrado(request: Request, exc: PedidoNaoEncontrado
     return JSONResponse(status_code=HTTPStatus.NOT_FOUND, content={"message": "Pedido não encontrado"})
 
 
-@app.get("/orders/{identificacao_do_pedido}/items")
-def listar_itens(itens: list[Item] = Depends(recuperar_itens_por_pedido)):
-    return itens
-
-
 @app.exception_handler(FalhaDeComunicacaoError)
 def tratar_erro_falha_de_comunicacao(request: Request, exc: FalhaDeComunicacaoError):
     return JSONResponse(status_code=HTTPStatus.BAD_GATEWAY, content={"message": "Falha de comunicação com o servidor remoto"})
 
 
-@app.get("/healthcheck")
+@app.get("/healthcheck", tags=["healthcheck"], summary="Integridade do sistema", description="Checa se o servidor está online", response_model=HealthCheckResponse)
 async def healthcheck():
-    return {"status": "ok"}
+    return HealthCheckResponse(status="ok")
+
+
+@app.get("/orders/{identificacao_do_pedido}/items", summary="Itens de um pedido", tags=["pedidos"], description="Retorna todos os itens de um determinado pedido", response_model=list[Item])
+def listar_itens(itens: list[Item] = Depends(recuperar_itens_por_pedido)):
+    return itens
+
+
+@app.get("/orders/{identificacao_do_pedido}/items", responses={
+    HTTPStatus.NOT_FOUND.value: {
+        "description": "Pedido não encontrado",
+        "model": ErrorResponse,
+    },
+    HTTPStatus.BAD_GATEWAY.value: {
+        "description": "Falha de comunicação com o servidor remoto",
+        "model": ErrorResponse,
+    }}, summary="Itens de um pedido", tags=["pedidos"], description="Retorna todos os itens de um determinado pedido", response_model=list[Item])

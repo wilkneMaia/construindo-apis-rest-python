0ce18228bb422e8b3abc8ca22974e21d467847ce
Wilkne Maia <wilkne@outlook.com>
2022-03-29 17:00:34 -0300

    Modifica api para responder de forma assíncrona

--- api_pedidos/api.py
+++ api_pedidos/api.py
@@ -10,6 +10,8 @@ from api_pedidos.excecao import (
     PedidoNaoEncontradoError,
 )
 
+# from api_pedidos.magalu_api_async import recuperar_itens_por_pedido
+
 app = FastAPI()
 
 
@@ -18,31 +20,64 @@ def recuperar_itens_por_pedido(identificacao_do_pedido: UUID) -> list[Item]:
 
 
 @app.exception_handler(PedidoNaoEncontradoError)
-def tratar_erro_pedido_nao_encontrado(request: Request, exc: PedidoNaoEncontradoError):
+async def tratar_erro_pedido_nao_encontrado(
+        request: Request, exc: PedidoNaoEncontradoError):
     return JSONResponse(status_code=HTTPStatus.NOT_FOUND, content={"message": "Pedido não encontrado"})
 
 
 @app.exception_handler(FalhaDeComunicacaoError)
-def tratar_erro_falha_de_comunicacao(request: Request, exc: FalhaDeComunicacaoError):
+async def tratar_erro_falha_de_comunicacao(
+    request: Request, exc: FalhaDeComunicacaoError
+):
     return JSONResponse(status_code=HTTPStatus.BAD_GATEWAY, content={"message": "Falha de comunicação com o servidor remoto"})
 
 
-@app.get("/healthcheck", tags=["healthcheck"], summary="Integridade do sistema", description="Checa se o servidor está online", response_model=HealthCheckResponse)
+@app.get(
+    "/healthcheck",
+    tags=["healthcheck"],
+    summary="Integridade do sistema",
+    description="Checa se o servidor está online",
+    response_model=HealthCheckResponse,
+)
 async def healthcheck():
     return HealthCheckResponse(status="ok")
 
 
-@app.get("/orders/{identificacao_do_pedido}/items", summary="Itens de um pedido", tags=["pedidos"], description="Retorna todos os itens de um determinado pedido", response_model=list[Item])
+@app.get("/orders/{identificacao_do_pedido}/items",
+         summary="Itens de um pedido",
+         tags=["pedidos"],
+         description="Retorna todos os itens de um determinado pedido",
+         response_model=list[Item])
 def listar_itens(itens: list[Item] = Depends(recuperar_itens_por_pedido)):
     return itens
 
 
-@app.get("/orders/{identificacao_do_pedido}/items", responses={
-    HTTPStatus.NOT_FOUND.value: {
-        "description": "Pedido não encontrado",
-        "model": ErrorResponse,
-    },
-    HTTPStatus.BAD_GATEWAY.value: {
-        "description": "Falha de comunicação com o servidor remoto",
-        "model": ErrorResponse,
-    }}, summary="Itens de um pedido", tags=["pedidos"], description="Retorna todos os itens de um determinado pedido", response_model=list[Item])
+# @app.get(
+#     "/orders/{identificacao_do_pedido}/items",
+#     responses={
+#         HTTPStatus.NOT_FOUND.value: {
+#             "description": "Pedido não encontrado",
+#             "model": ErrorResponse,
+#         },
+#         HTTPStatus.BAD_GATEWAY.value: {
+#             "description": "Falha de comunicação com o servidor remoto",
+#             "model": ErrorResponse,
+#         },
+#     },
+#     summary="Itens de um pedido",
+#     tags=["pedidos"],
+#     description="Retorna todos os itens de um determinado pedido",
+#     response_model=list[Item],
+# )
+# async def listar_itens(itens: list[Item] = Depends(recuperar_itens_por_pedido)):
+#   v return itens
+
+# @app.get("/orders/{identificacao_do_pedido}/items", responses={
+#     HTTPStatus.NOT_FOUND.value: {
+#         "description": "Pedido não encontrado",
+#         "model": ErrorResponse,
+#     },
+#     HTTPStatus.BAD_GATEWAY.value: {
+#         "description": "Falha de comunicação com o servidor remoto",
+#         "model": ErrorResponse,
+#     }}, summary="Itens de um pedido", tags=["pedidos"], description="Retorna todos os itens de um determinado pedido", response_model=list[Item])
